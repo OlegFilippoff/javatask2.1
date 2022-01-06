77e60a26962fffba3aed5af6ea2be2aaaadc5e26
Oleg Filippov <5157175@bk.ru>
2022-01-06 20:52:00 +0300

    Main class rename

--- src/main.java
+++ src/main.java
@@ -1,7 +1,7 @@
-public class main {
+public class Main {
     public static void main(String[] args) {
-        int ticketCost = 20;
-        int ticketQuantity = 2;
+        int ticketCost = 2000;
+        int ticketQuantity = 1;
         int totalBonusMiles = ticketCost*ticketQuantity/20;
         System.out.println("Сумма начисленых бонусов: " + totalBonusMiles + " миль(и)");
     }
