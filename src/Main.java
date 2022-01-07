10536b7726cd78074dcd410ff04f42d537070a06
Oleg Filippov <5157175@bk.ru>
2022-01-07 17:01:27 +0300

    reformatting code

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int ticketCost = 2000;
         int ticketQuantity = 1;
-        int totalBonusMiles = ticketCost*ticketQuantity/20;
+        int totalBonusMiles = ticketCost * ticketQuantity / 20;
         System.out.println("Сумма начисленых бонусов: " + totalBonusMiles + " миль(и)");
     }
 }
